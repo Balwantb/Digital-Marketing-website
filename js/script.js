4107f64e9487a753c572b721c6efd39588bd9c0a
Balwant <bbirajdar7@gmail.com>
2020-06-12 19:37:06 +0530

    fourth section inprogress

--- js/script.js
+++ js/script.js
@@ -76,3 +76,53 @@ window.onload = function () {
         count($(this));
     });
 })(jQuery);
+
+/*smoth scroling*/
+
+$(document).ready(function () {
+    $('.menu a').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target
+                || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body')
+                    .animate({ scrollTop: targetOffset }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
+//progress bar js
+
+$(document).ready(function ($) {
+    function animateElements() {
+        $('.progressbar').each(function () {
+            var elementPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            var percent = $(this).find('.circle').attr('data-percent');
+            var animate = $(this).data('animate');
+            if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+                $(this).data('animate', true);
+                $(this).find('.circle').circleProgress({
+                    // startAngle: -Math.PI / 2,
+                    value: percent / 100,
+                    size: 400,
+                    thickness: 15,
+                    fill: {
+                        color: '#f47a14'
+                    }
+                }).on('circle-animation-progress', function (event, progress, stepValue) {
+                    $(this).find('strong').text((stepValue * 100).toFixed(0) + "%");
+                }).stop();
+            }
+        });
+    }
+
+    animateElements();
+    $(window).scroll(animateElements);
+});
